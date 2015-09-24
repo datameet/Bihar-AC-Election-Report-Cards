ef8d3bc562a198cc540314aa01e3ec4e6c181861
thejeshgn <i@thejeshgn.com>
2015-09-24 19:53:05 +0530

    Added Khagaria_Aalauli_nrdwp.csv

--- code/data/merge.py
+++ code/data/merge.py
@@ -12,6 +12,7 @@ import dataset
 import time
 import datetime
 from os import listdir
+import csv
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
@@ -22,7 +23,7 @@ def find_csv_filenames( path_to_dir, suffix=".csv" ):
 if __name__ == '__main__':
 	options = ArgumentParser()
 	options.add_argument('-f', '--folder', action='store', dest='folder', help='Provide the folder path of csv')
-	options.add_argument('-o', '--output', action='store', dest='out',  help='whats the output csv name?')
+	options.add_argument('-o', '--output', action='store', dest='output',  help='whats the output csv name?')
 	args 	= options.parse_args()
 	folder 	= args.folder
 	output	= args.output
@@ -38,28 +39,34 @@ if __name__ == '__main__':
 		col_no = 0
 		for named_column in named_columns:
 			all_named_columns["col_no"+str(col_no)] = str(named_column).replace("_", " ")
+			col_no = col_no+1
+		print "all_named_columns="+str(all_named_columns)
 		row_no = 0
+		csv_file = str(folder)+"/"+csv_file
+		print "csv_file"+str(csv_file)
 		with open(csv_file, 'rb') as csvfile:
 			reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 			for row in reader:
+				print ', '.join(row)
 				if done_once == False:
 					for col in row:
 						column_names.append(col)
 					done_once = True
+					print "Done once the header"
+					row_no = row_no + 1
 					continue
 				if row_no == 0:
 					row_no = row_no + 1
 					continue
-					
+				row_no = row_no + 1
+				print "row_no="+str(row_no)
+				
 				insert_data = {}
 				insert_data.update(all_named_columns)
 				col_no = 0
 				for col in row:
 					insert_data[column_names[col_no]] = col
 					col_no = col_no+1
+				print "insert_data"+str(insert_data)
 				table.insert(dict(insert_data))
 				db.commit()
-		
-		
-			
-		
---
+++ code/data/readme.txt
@@ -0,0 +1,2 @@
+cd Bihar-AC-Election-Report-Cards
+python merge.py -f ./../../data/in_processing/khagaria/Khagaria-Alauli/national_rural_drinking_water_programme -o alauli_nrdw
